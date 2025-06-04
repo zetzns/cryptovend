94a0168d944994a6e9567277987f1a734a438990
Zakurin Alexander <115410511+zetzns@users.noreply.github.com>
2025-06-04 16:37:12 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.21",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    sepolia: {
+      url: "https://rpc2.sepolia.org",   // публичный RPC
+      accounts: [
+        // приватник первого аккаунта Hardhat – годится ТОЛЬКО для тестов!
+        "0x59c6995e998f97a5a00449736f5dc00e10953340"
+      ]
+    }
+  }
+};
+`
